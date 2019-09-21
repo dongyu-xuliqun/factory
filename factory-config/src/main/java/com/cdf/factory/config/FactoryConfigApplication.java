66470978681e44a5cc67b35a03c52167e37a133f
dongyu-xuliqun <xuliqunpersonal@hotmail.com>
2019-09-21 09:50:47 +0800

    spring boot admin 监控添加security配置 由于目前貌似没有加载到项目里的application.yml配置,暂时先将application.yml里的配置移到bootstrap.yml里，规避取不到值得问题

--- factory-config/src/main/java/com/cdf/factory/config/FactoryConfigApplication.java
+++ factory-config/src/main/java/com/cdf/factory/config/FactoryConfigApplication.java
@@ -1,13 +1,11 @@
 package com.cdf.factory.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigServer
 @EnableDiscoveryClient
 public class FactoryConfigApplication {
